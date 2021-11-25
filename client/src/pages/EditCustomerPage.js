ef095c4205ba89303bd1779dbc06b5d852d53f65
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-11-25 18:52:40 +0100

    create a page/form to edit the customer data

---
+++ client/src/pages/EditCustomerPage.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export default function EditCustomerPage(props) {
+    
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [gender, setGender] = useState("");
+    const [street, setStreet] = useState("");
+    const [postalCode, setPostalCode] = useState("");
+    const [city, setCity] = useState("");
+    const customerId = props.match.params.id;
+    
+    useEffect(() => {
+        axios.get(`/api/customers/${customerId}`)
+        .then(res => {
+            setFirstName(res.data.firstName);
+            setLastName(res.data.lastName);
+            setGender(res.data.gender);
+            setStreet(res.data.street);
+            setPostalCode(res.data.postalCode);
+            setCity(res.data.city);
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const reqBody = { firstName, lastName, gender, street, postalCode, city };
+        axios.put(`/api/customers/${customerId}`, reqBody)
+            .then(res => {
+                props.history.push('/');
+            })
+            .catch(err => console.log(err))
+
+    }
+
+    return (
+        <div>
+            <h3>Edit Customer</h3>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input 
+                    type="text"
+                    name="firstName"
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                />
+                <label htmlFor="lastName">Last Name</label>
+                <input 
+                    type="text"
+                    name="lastName"
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)}
+                />
+                <label htmlFor="gender">Gender</label>
+                <input 
+                    type="text"
+                    name="gender"
+                    value={gender}
+                    onChange={e => setGender(e.target.value)}
+                />
+                <label htmlFor="street">Street</label>
+                <input 
+                    type="text"
+                    name="street"
+                    value={street}
+                    onChange={e => setPostalCode(e.target.value)}
+                />
+                <label htmlFor="postalCode">Postal Code</label>
+                <input 
+                    type="text"
+                    name="postalCode"
+                    value={postalCode}
+                    onChange={e => setPostalCode(e.target.value)}
+                />
+                <label htmlFor="city">City</label>
+                <input 
+                    type="text"
+                    name="city"
+                    value={city}
+                    onChange={e => setCity(e.target.value)}
+                />
+                <button type="submit">Update This Customer</button>
+            </form>
+        </div>
+    )
+}
