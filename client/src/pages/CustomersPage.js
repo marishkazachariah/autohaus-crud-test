99b7ff99d5ab4b57ba672711ca506906be856bf0
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-11-26 16:00:38 +0100

    rework CustomersPage/Homepage so it maps out the backend data via the CustomerCard component

--- client/src/pages/CustomersPage.js
+++ client/src/pages/CustomersPage.js
@@ -1,36 +1,24 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
-import service from '../api/service';
-import { Link } from 'react-router-dom';
+import CustomerCard from '../components/CustomerCard';
 
 export default function CustomersPage(props) {
 
-    const [customer, setCustomer] = useState([]);
-    //customerId = props.match.params.id;
+    const [customers, setCustomer] = useState([]);
 
-    const getCustomerDetails = () => {
+    const getAllCustomers = () => {
         axios.get(`/api/customerdata`)
             .then(res => {
-                console.log("customer data:", res.data);
+                // console.log(res.data);
                 setCustomer(res.data);
             })
             .catch(err => console.log(err))
     }
     
     useEffect(() => {
-        getCustomerDetails();
+        getAllCustomers();
     }, [])
-
-    const deleteCustomer = (id) => {
-        return service
-        .deleteCustomer(id)
-        .then(res => {
-            console.log('customer deleted', res);
-            props.history.push('/');
-        })
-        .catch(err => console.log(err))
-    }
 
     return (
         <div>
@@ -47,22 +35,7 @@ export default function CustomersPage(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        {customer && (
-                            <>
-                                <td>{customer.firstName}</td>
-                                <td>{customer.lastName}</td>
-                                <td>{customer.gender}</td>
-                                <td>{customer.street}</td>
-                                <td>{customer.postalCode}</td>
-                                <td>{customer.city}</td>
-                                <td>
-                                    <button onClick={() => deleteCustomer(customer._id)}>Delete</button>
-                                    <Link to={`/customers/edit/${customer._id}`}><button>Edit</button></Link>
-                                </td>
-                            </>
-                        )}
-                    </tr>
+                    {customers.map(customer => <CustomerCard key={customer._id} {...customer} {...props} />)}
                 </tbody>
             </table>
         </div>
