8b9c2abdb92c3b3c50b47a4a29023f9cbc3a80c2
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-11-26 16:01:41 +0100

    bug fix to GET customerdata so it renders on CustomersPage

--- server/routes/customerdata.js
+++ server/routes/customerdata.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Customer = require("../models/Customer");
 
-router.get("/", (res, next) => {
+router.get("/", (req, res, next) => {
     Customer.find()
     .then((customerData) => {
         console.log(customerData);
